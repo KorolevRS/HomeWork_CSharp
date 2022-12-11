cf65927ad21a906cb22be7aa041c1df1747c2a0e
Roman <romeoone@mail.ru>
2022-12-11 17:46:24 +0300

    Задача 43

--- Program.cs
+++ Program.cs
@@ -1,43 +1,15 @@
-﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3*/
-int m, x;
-int kol = 0;
-// Console.WriteLine($"Сколько чисел выхотите вести? ");
-// int.TryParse(Console.ReadLine()!, out m);
-// int[] massive = new int[m];
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+int k1, k2, b1, b2;
+Console.WriteLine($"Введите коэфициенты для прямых: b1, k1, b2, k2");
+string[] ratioS = Console.ReadLine()!.Split(',');
 
-// for (int i = 0; i < m; i++)
-// {
-//     Console.WriteLine($"Введите {i + 1}-е число:");
-//     int.TryParse(Console.ReadLine()!, out x);
-//     massive[i] = x;
-//     if (x > 0) kol++;
-// }
-// Print(massive);
-// Console.WriteLine($"количество чисел больше -> {kol} ");
-
-// void Print(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//         Console.Write($"{arr[i]} ");
-// }
-// // Решение с помощью отдельного/раздельного ввода*/
-
-Console.WriteLine($"Введите числа через ','");
-string[] massiveS = Console.ReadLine()!.Split(',');
-
-for (int i = 0; i < massiveS.Length; i++)
-{
-    int.TryParse(massiveS[i], out x);
-    if (x > 0) kol++;
-} //Решение с помощью считывания через Split
-
-Print(massiveS);
-Console.WriteLine($"количество чисел больше -> {kol} ");
-
-void Print(string[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-        Console.Write($"{arr[i]} ");
-}
+int.TryParse(ratioS[0], out b1);
+int.TryParse(ratioS[1], out k1);
+int.TryParse(ratioS[2], out b2);
+int.TryParse(ratioS[3], out k2);
+if (k1 == k2)
+    Console.WriteLine($"Прямые не пересекаются.");
+else
+    Console.WriteLine($"Две прямые с коэффициентами b1={b1}, k1={k1}, b1={b2}, k2={k2} пересекутся в точке с координатами {(double)(b2 - b1) / (k1 - k2)}, {(double)k1 * (b2 - b1) / (k1 - k2) + b1}");
