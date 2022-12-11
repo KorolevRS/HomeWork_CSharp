c3bbda6daedc976b7d2c1aef59ec3876ae3bbe7a
Roman <romeoone@mail.ru>
2022-12-11 16:09:05 +0300

    Исправил задачу 38

--- Program.cs
+++ Program.cs
@@ -3,12 +3,11 @@
 [3 7 22 2 78] -> 76*/
 
 double[] array = new double[6];
-
+//double[] array = new double[6] { 5, 5.6, 9, 8, 1.2, 45 }; так мы объявим массив в явном виде
 for (int i = 0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(-100, 100);
+    array[i] = new Random().Next(-100, 100) / 10.0;
+// array[i] = new Random().Next(-100, 100);
 
-}
 double min = array[0];
 double max = array[0];
 Print(array);
@@ -22,9 +21,7 @@ void MinMax(double[] arr1)
         if (arr1[i] >= max) max = arr1[i];
         else if ((arr1[i] < min)) min = arr1[i];
     }
-
 }
-
 
 void Print(double[] arr)
 {
