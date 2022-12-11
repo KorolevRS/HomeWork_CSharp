bf714085e5b60b3227f977ecce88553b5d023363
Roman <romeoone@mail.ru>
2022-12-11 15:59:42 +0300

    Исправил задачу 34

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ int[] array = new int[3];
 int k = 0;
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(0, 1000);
+    array[i] = new Random().Next(100, 1000);
     if (array[i] % 2 == 0) k++;
 }
 Print(array);
