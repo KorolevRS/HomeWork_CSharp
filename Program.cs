af0ea5e01c6c4d00ee476e604fa2de0460cd973d
Roman <romeoone@mail.ru>
2022-12-12 23:42:55 +0300

    Задача 50

--- Program.cs
+++ Program.cs
@@ -1,43 +1,33 @@
-﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3*/
-int m, x;
-int kol = 0;
-// Console.WriteLine($"Сколько чисел выхотите вести? ");
-// int.TryParse(Console.ReadLine()!, out m);
-// int[] massive = new int[m];
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+Console.Write($"Введите номер элемента массива в формате x,y: ");
+string[] vvodEl = Console.ReadLine()!.Split(',');
+int x, y;
+int.TryParse(vvodEl[0], out x);
+int.TryParse(vvodEl[1], out y);
+int[,] array = new int[4, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = new Random().Next(0, 11);
 
-// for (int i = 0; i < m; i++)
-// {
-//     Console.WriteLine($"Введите {i + 1}-е число:");
-//     int.TryParse(Console.ReadLine()!, out x);
-//     massive[i] = x;
-//     if (x > 0) kol++;
-// }
-// Print(massive);
-// Console.WriteLine($"количество чисел больше -> {kol} ");
+Print(array);
 
-// void Print(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//         Console.Write($"{arr[i]} ");
-// }
-// // Решение с помощью отдельного/раздельного ввода*/
+if (x <= array.GetLength(0) && y <= array.GetLength(1)) Console.WriteLine($"{array[x - 1, y - 1]}");
+else Console.WriteLine($"Элемента с номером ({x},{y}) отсутствует");
 
-Console.WriteLine($"Введите числа через ','");
-string[] massiveS = Console.ReadLine()!.Split(',');
-
-for (int i = 0; i < massiveS.Length; i++)
+void Print(int[,] arr)
 {
-    int.TryParse(massiveS[i], out x);
-    if (x > 0) kol++;
-} //Решение с помощью считывания через Split
-
-Print(massiveS);
-Console.WriteLine($"количество чисел больше 0 -> {kol} ");
-
-void Print(string[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-        Console.Write($"{arr[i]} ");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
 }
