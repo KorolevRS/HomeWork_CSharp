08aba14a92be20d6ef2416b6509e3d066076caf5
Roman <romeoone@mail.ru>
2022-12-12 22:17:01 +0300

    не дописал

--- Program.cs
+++ Program.cs
@@ -1,43 +1,17 @@
-﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3*/
-int m, x;
-int kol = 0;
-// Console.WriteLine($"Сколько чисел выхотите вести? ");
-// int.TryParse(Console.ReadLine()!, out m);
-// int[] massive = new int[m];
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+int m, n;
+Console.Write($"Введите размерность m: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.Write($"Введите размерность m: ");
+int.TryParse(Console.ReadLine()!, out n);
 
-// for (int i = 0; i < m; i++)
-// {
-//     Console.WriteLine($"Введите {i + 1}-е число:");
-//     int.TryParse(Console.ReadLine()!, out x);
-//     massive[i] = x;
-//     if (x > 0) kol++;
-// }
-// Print(massive);
-// Console.WriteLine($"количество чисел больше -> {kol} ");
-
-// void Print(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//         Console.Write($"{arr[i]} ");
-// }
-// // Решение с помощью отдельного/раздельного ввода*/
-
-Console.WriteLine($"Введите числа через ','");
-string[] massiveS = Console.ReadLine()!.Split(',');
-
-for (int i = 0; i < massiveS.Length; i++)
+void PrintMassive(int[] arr)
 {
-    int.TryParse(massiveS[i], out x);
-    if (x > 0) kol++;
-} //Решение с помощью считывания через Split
-
-Print(massiveS);
-Console.WriteLine($"количество чисел больше 0 -> {kol} ");
-
-void Print(string[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-        Console.Write($"{arr[i]} ");
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write(arr[i, j])
 }
