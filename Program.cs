1e935299673a56cd0bcc976459c6824983fe2016
Roman <romeoone@mail.ru>
2022-12-12 23:04:15 +0300

    Задача 47

--- Program.cs
+++ Program.cs
@@ -6,12 +6,22 @@ m = 3, n = 4.
 int m, n;
 Console.Write($"Введите размерность m: ");
 int.TryParse(Console.ReadLine()!, out m);
-Console.Write($"Введите размерность m: ");
+Console.Write($"Введите размерность n: ");
 int.TryParse(Console.ReadLine()!, out n);
+double[,] array = new double[m, n];
 
-void PrintMassive(int[] arr)
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = (double)new Random().Next(-100, 100) / 10;
+
+PrintMassive(array);
+
+void PrintMassive(double[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
+    {
         for (int j = 0; j < arr.GetLength(1); j++)
-            Console.Write(arr[i, j])
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
 }
