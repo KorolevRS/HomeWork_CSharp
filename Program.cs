e46453e4d09c7f3a3d541763467e48182e2dfdde
Roman <romeoone@mail.ru>
2022-12-14 22:53:50 +0300

    Задача55 исправлена

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@
 3 2 | 3 3
 Результирующая матрица будет:
 18 20
-15 18*/
-int[,] array1 = { { 2, 4 }, { 3, 2 } };
-int[,] array2 = { { 3, 4 }, { 3, 3 } };
-int[,] arrayResult = new int[2, 2];
+15 18
+Работает только для квадратных матриц одного размера*/
+int[,] array1 = new int[2, 2];
+int[,] array2 = new int[2, 2];
+int[,] arrayResult = new int[array1.GetLength(0), array2.GetLength(1)];
+FillArray(array1);
 Print(array1);
 Console.WriteLine();
-
+FillArray(array2);
 Print(array2);
 Console.WriteLine();
 
@@ -19,17 +21,17 @@ for (int i = 0; i < arrayResult.GetLength(0); i++)
         arrayResult[i, j] = ProizvedenieElemetov(array1, array2, i, j);
 Print(arrayResult);
 
-// void FillArray(int[,] arr)//заполнение массива
-// {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < arr.GetLength(1); j++)
-//         {
-//             arr[i, j] = new Random().Next(10);
-//         }
+void FillArray(int[,] arr)//заполнение массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(10);
+        }
 
-//     }
-// }
+    }
+}
 void Print(int[,] arr)//вывод массива
 {
     for (int i = 0; i < arr.GetLength(0); i++)
