6ad10f8492627a6086da01412ea26906bbd3dba2
Roman <romeoone@mail.ru>
2022-12-14 22:55:41 +0300

    Задача62

--- Program.cs
+++ Program.cs
@@ -1,33 +1,18 @@
-﻿/*Задача 52. Задайте двумерный массив из целых чисел. 
-Найдите среднее арифметическое элементов в каждом столбце.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
 int[,] array = new int[3, 4];
 for (int i = 0; i < array.GetLength(0); i++)
     for (int j = 0; j < array.GetLength(1); j++)
         array[i, j] = new Random().Next(0, 10);
 
-double result;
-Print(array);
-SredArifmet(array);
-Console.WriteLine();
 
-void SredArifmet(int[,] arr)
-{
-    Console.Write($"Среднее арифметическое каждого столбца ");
-    for (int i = 0; i < array.GetLength(1); i++)
-    {
-        double sum = 0;
-        for (int j = 0; j < array.GetLength(0); j++)
-        {
-            sum += array[j, i];
-        }
-        Console.Write($"{Math.Round(result = sum / array.GetLength(0), 2)}; ");
-    }
-}
+
+
 
 void Print(int[,] arr)
 {
