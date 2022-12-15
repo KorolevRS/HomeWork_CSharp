2eb5e89ddb20395edc4c8f36f1d2f3046dfbcff9
Roman <romeoone@mail.ru>
2022-12-15 23:40:18 +0300

    Задача 60

--- Program.cs
+++ Program.cs
@@ -6,24 +6,34 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)*/
 int[,,] array = new int[2, 2, 2];
+int[] nums = new int[90];
+for (int i = 0; i < 90; i++)
+    nums[i] = i + 10;
+
 for (int i = 0; i < array.GetLength(0); i++)
     for (int j = 0; j < array.GetLength(1); j++)
         for (int k = 0; k < array.GetLength(2); k++)
-            array[i, j, k] = new Random().Next(10, 99);
-
+        {
+            int temp = new Random().Next(90);
+            while (nums[temp] == 0)
+                temp = new Random().Next(90);
+            array[i, j, k] = nums[temp];
+            nums[temp] = 0;
+        }
 
 Print(array);
 Console.WriteLine();
 
 
-
-void Print(int[,,] arr)
+void Print(int[,,] arr)//вывод массива
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
-            for (int k = 0; j < arr.GetLength(2); k++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
                 Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
-        Console.WriteLine();
+            Console.WriteLine();
+        }
     }
 }
