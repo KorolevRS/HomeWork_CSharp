2e4785e78f797b02a954162a5f92fa8228973109
Roman <romeoone@mail.ru>
2022-12-16 23:21:25 +0300

    Задача66

--- Program.cs
+++ Program.cs
@@ -1,48 +1,25 @@
-﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
-каждой строки двумерного массива.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2*/
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+ сумму натуральных элементов в промежутке от M до N.
 
-int[,] array = new int[3, 4];
-FillArray(array);
-PrintMassive(array);
-Console.WriteLine();
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        for (int k = 0; k < array.GetLength(1) - 1; k++)
-        {
-            if (array[i, k] < array[i, k + 1])
-            {
-                int temp = array[i, k];
-                array[i, k] = array[i, k + 1];
-                array[i, k + 1] = temp;
-            }
-        }
-    }
-}
-PrintMassive(array);
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
 
-void PrintMassive(int[,] arr) //вывод на экран массива
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-            Console.Write($"{arr[i, j]} ");
-        Console.WriteLine();
-    }
-}
+int m, n;
+Console.WriteLine($"Введите значение M: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.WriteLine($"Введите значение N: ");
+int.TryParse(Console.ReadLine()!, out n);
+int sum = 0;
 
-void FillArray(int[,] arr) //Заполнение массива
+if (m > n) Summa(m, n);
+else Summa(n, m);
+Console.WriteLine($"M = {m}; N = {n} -> {sum}");
+
+int Summa(int big, int little)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
-        for (int j = 0; j < arr.GetLength(1); j++)
-            arr[i, j] = new Random().Next(10);
+    if (big < little)
+        return sum;
+    sum += big;
+    Summa(big - 1, little);
+    return sum;
 }
