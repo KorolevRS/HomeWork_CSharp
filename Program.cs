828ed6439078eef8ce4ee7bb65808ea702f332fa
Roman <romeoone@mail.ru>
2022-12-16 22:52:30 +0300

    Задача64

--- Program.cs
+++ Program.cs
@@ -1,48 +1,22 @@
-﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
-каждой строки двумерного массива.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-В итоге получается вот такой массив:
-7 4 2 1
-9 5 3 2
-8 4 4 2*/
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-int[,] array = new int[3, 4];
-FillArray(array);
-PrintMassive(array);
-Console.WriteLine();
-for (int i = 0; i < array.GetLength(0); i++)
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+int n;
+Console.WriteLine($"Введите значение N: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+PrintChisla(n);
+
+void PrintChisla(int k)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    if (k <= 1)
+        Console.WriteLine($"{k}");
+    else
     {
-        for (int k = 0; k < array.GetLength(1) - 1; k++)
-        {
-            if (array[i, k] < array[i, k + 1])
-            {
-                int temp = array[i, k];
-                array[i, k] = array[i, k + 1];
-                array[i, k + 1] = temp;
-            }
-        }
+        Console.Write($"{k}, ");
+        PrintChisla(k - 1);
     }
-}
-PrintMassive(array);
 
-void PrintMassive(int[,] arr) //вывод на экран массива
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-            Console.Write($"{arr[i, j]} ");
-        Console.WriteLine();
-    }
-}
-
-void FillArray(int[,] arr) //Заполнение массива
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-        for (int j = 0; j < arr.GetLength(1); j++)
-            arr[i, j] = new Random().Next(10);
 }
